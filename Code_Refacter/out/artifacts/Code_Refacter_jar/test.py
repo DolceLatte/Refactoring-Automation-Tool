85252c6545a8e7e5640aed693dbc1339aecd1972
201502038 <bnm961126@gmail.com>
2020-01-16 15:34:18 +0900

    test jar file

---
+++ Code_Refacter/out/artifacts/Code_Refacter_jar/test.py
@@ -0,0 +1,16 @@
+"""
+sda
+"""
+a = 10
+b = 10
+c = 10
+
+class hello():
+    def __init__(self):
+        self._val = 0
+
+    def add(self, c, d):
+        self._val = c + d
+        return c + d
+
+h = hello()
