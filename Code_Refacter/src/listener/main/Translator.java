fb0ceca63b8cc2cc316aa70466cde41de7360591
201502038 <bnm961126@gmail.com>
2020-01-16 15:27:56 +0900

    can input file with args

--- Code_Refacter/src/listener/main/Translator.java
+++ Code_Refacter/src/listener/main/Translator.java
@@ -7,25 +7,17 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.*;
 
+import java.util.Arrays;
+
 public class Translator extends ParseTreeWalker {
-    enum OPTIONS {
-        PYTHON3CODEGEN, ERROR
-    }
-
-    private static OPTIONS getOption(String[] args) {
-        if (args.length < 1)
-            return OPTIONS.PYTHON3CODEGEN;
-
-        if (args[0].startsWith("-b")
-                || args[0].startsWith("-B"))
-            return OPTIONS.PYTHON3CODEGEN;
-
-        return OPTIONS.ERROR;
-    }
 
     public static void main(String[] args) throws Exception {
-//        CharStream codeCharStream = CharStreams.fromFileName(Arrays.toString(args));
-        CharStream codeCharStream = CharStreams.fromFileName("test.py");
+        StringBuilder builder = new StringBuilder();
+        for(String s : args) {
+            builder.append(s);
+        }
+        String str = builder.toString();
+        CharStream codeCharStream = CharStreams.fromFileName(str);
         Python3Lexer lexer = new Python3Lexer(codeCharStream);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         Python3Parser parser = new Python3Parser(tokens);
@@ -34,12 +26,8 @@ public class Translator extends ParseTreeWalker {
 
         ParseTreeWalker walker = new ParseTreeWalker();
         ParseTree tree = parser.decl();
-        switch (getOption(args)) {
-            case PYTHON3CODEGEN:
-                walker.walk(new Python3codeGenListener(), tree);
-                break;
-            default:
-                break;
-        }
+
+        walker.walk(new Python3codeGenListener(), tree);
+
     }
 }
